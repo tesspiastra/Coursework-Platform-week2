4917a00825368f05a87d4afc04c69fc508c58785
tesspiastra <tpiastra246@gmail.com>
2025-03-04 11:04:27 +0000

    OOP progress on the country searcher

--- countries/dist/countries.js
+++ countries/dist/countries.js
@@ -1,8 +1,47 @@
 import axios from 'axios';
+import { input } from '@inquirer/prompts';
 const COUNTRY_URL = "https://restcountries.com/v3.1/name/";
-let getCountry = async (country) => {
-    const response = await axios.get(`${COUNTRY_URL}${country}`);
-    const allData = response.data[0];
-    console.log(`Country name: ${allData["name"]["common"]}${allData["flags"]["png"]}\nCurrency: ${allData["currencies"]}\nLanguage(s):${allData["languages"]}\nContinent: ${allData["continents"][0]}`);
-};
-getCountry("france");
+class Country {
+    name;
+    flag;
+    currency;
+    language;
+    continent;
+    population;
+    constructor(name, flag, currency, language, continent, population) {
+        this.name = name;
+        this.flag = flag;
+        this.currency = currency;
+        this.language = language;
+        this.continent = continent;
+        this.population = population;
+    }
+    showCountry() {
+        console.log(`Country name: ${this.name} ${this.flag}\nCurrency: ${this.currency}\nLanguage(s): ${this.language}\nContinent: ${this.continent}\nPopulation: ${this.population} people`);
+    }
+}
+class App {
+    constructor() { }
+    async getCountry(country) {
+        const response = await axios.get(`${COUNTRY_URL}${country}`);
+        const allData = response.data[0];
+        const name = allData["name"]["common"];
+        const flag = allData["flag"];
+        const currCode = Object.keys(allData["currencies"])[0];
+        const currencies = allData["currencies"][currCode]?.name;
+        const language = allData["languages"][Object.keys(allData["languages"])[0]];
+        const continent = allData["continents"][0];
+        const population = allData["population"];
+        const newCountry = new Country(name, flag, currencies, language, continent, population);
+        newCountry.showCountry();
+    }
+    async startSession() {
+        let search = await input({ message: "What country do you want to search for? (type 'exit' to quit interface)" });
+        while (search != 'exit') {
+            await this.getCountry(search);
+            search = await input({ message: "You may search again:" });
+        }
+    }
+}
+let app = new App;
+app.startSession();
--- countries/src/countries.ts
+++ countries/src/countries.ts
@@ -1,11 +1,60 @@
 import axios from 'axios';
+import {input} from '@inquirer/prompts';
 
 const COUNTRY_URL = "https://restcountries.com/v3.1/name/"
 
-let getCountry = async (country: string)=>{
-    const response = await axios.get(`${COUNTRY_URL}${country}`)
-    const allData = response.data[0]
-    console.log(allData)
-    console.log(`Country name: ${allData["name"]["common"]}${allData["flags"]["png"]}\nCurrency: ${allData["currencies"]}\nLanguage(s):${allData["languages"]}\nContinent: ${allData["continents"][0]}`)
+class APIerror{
+    // not implemented not
 }
-getCountry("france")
+
+class Country {
+    name: string;
+    flag: string;
+    currency: string;
+    language: string;
+    continent: string;
+    population: string;
+
+    constructor(name: string,flag: string,currency: string,language: string,continent: string, population: string){
+        this.name = name
+        this.flag = flag
+        this.currency = currency
+        this.language = language
+        this.continent = continent
+        this.population = population
+    }
+    showCountry(){
+        console.log(`Country name: ${this.name} ${this.flag}\nCurrency: ${this.currency}\nLanguage(s): ${this.language}\nContinent: ${this.continent}\nPopulation: ${this.population} people`)
+    }
+}
+
+class App {
+    constructor(){}
+
+    async getCountry (country: string) {
+        const response = await axios.get(`${COUNTRY_URL}${country}`)
+        const allData = response.data[0]
+        
+        const name = allData["name"]["common"]
+        const flag = allData["flag"]
+        const currCode = Object.keys(allData["currencies"])[0]
+        const currencies = allData["currencies"][currCode as keyof typeof allData["currencies"]]?.name
+        const language = allData["languages"][Object.keys(allData["languages"])[0]]
+        const continent = allData["continents"][0]
+        const population = allData["population"]
+
+        const newCountry = new Country(name,flag,currencies,language,continent,population)
+        newCountry.showCountry()
+    }
+    async startSession (){
+        let search = await input({message: "What country do you want to search for? (type 'exit' to quit interface)"})
+        while (search != 'exit'){
+            await this.getCountry(search)
+            
+            search = await input({message: "You may search again:"})
+    }  
+    }
+}
+
+let app = new App
+app.startSession()
